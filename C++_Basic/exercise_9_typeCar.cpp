7db2498805a2d9212f0d3a0eaae7f10dad23e18b
ngoductho2001 <112944236+ngoductho2001@users.noreply.github.com>
2023-05-25 22:58:12 +0700

    exercise_9_typeCar.cpp

--- C++_Basic/exercise_9_typeCar.cpp
+++ C++_Basic/exercise_9_typeCar.cpp
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<stdint.h>
 #include<string>
-
 using namespace std;
 
 class Car{
@@ -11,27 +10,45 @@ class Car{
         int distance;
     public:
         Car();
-        void setCar();
-        void maintenanceCost();
+        int maintenanceCost();
         void checkDistance();
         void showInformationCar();
+        void setCar();
 };
 
 Car::Car(){
-    this -> color = " ";
-    this -> engine= " ";
-    this -> distance = 0;
+    this->color = "";
+    this->engine= "";
+    this->distance = 0;
 }
 
 void Car::setCar(){
     printf("Enter color of your car:");
-    scanf("%s", &(this -> color));
+    scanf("%s", &(this->color));
     printf("Enter engine of your car:");
-    scanf("%s", &(this -> engine));
+    scanf("%s", &(this->engine));
     printf("Enter the distance your car ha traveled:");
-    scanf("%d", &(this -> distance));
+    scanf("%d", &(this->distance));
+    
 }
 
-void Car::maintenanceCost(){
-    
+int Car::maintenanceCost(){
+    return distance * 12;
+}
+
+void Car::checkDistance(){
+    printf("Current kilometer: %d",this->distance);
+}
+
+void Car::showInformationCar(){
+    printf("Color: %s\n", this->color.c_str());
+    printf("Engine: %s\n", this->engine.c_str());
+    printf("Kilometer: %d\n", this->distance);
+}
+
+int main(){
+    Car car;
+    car.setCar();
+    car.showInformationCar();
+    car.checkDistance();
 }
